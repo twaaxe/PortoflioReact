7a304cb1fbbc5c20182cf493e3ec14c8aa4c4e79
twaaxe <35602018+twaaxe@users.noreply.github.com>
2023-05-16 16:14:34 +0200

    computer section (3d)

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload } from '@react-three/drei';
+import { useGLTF } from '@react-three/drei/core/useGLTF'
+import CanvasLoader from '../Loader'
 
 const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf")
   return (
-    <div>Computers</div>
+    <mesh>
+
+    </mesh>
   )
 }
 
