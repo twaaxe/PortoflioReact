33fd96de93cfe38db0d176a5cb10f3b286f163fb
twaaxe <35602018+twaaxe@users.noreply.github.com>
2023-05-21 10:40:27 +0200

    expericnce

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,6 +19,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
+      <p>computer canvvas</p>
       <ComputersCanvas />
       <div className='absolute xs:bottom-10  bottom-32 w-full flex justify-center items-center'>
         <a href="#about">
