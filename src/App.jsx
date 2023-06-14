865fced12c73c4373d35cb59f3b7887e5a932fdd
twaaxe <35602018+twaaxe@users.noreply.github.com>
2023-06-14 19:22:53 +0200

    major changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom"
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Works, StarsCanvas, Tech } from './components'
+import { About, Contact, Experience, Hero, Navbar, Works, StarsCanvas, Tech } from './components'
 
 const App = () => {
 
@@ -9,16 +9,16 @@ const App = () => {
         <div className="relative z-0 bg-primary">
           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
             <Navbar />
+            <StarsCanvas />
+
             <Hero />
           </div>
           <About />
           <Experience />
           <Tech />
           {/* <Works /> */}
-          {/* <Feedbacks /> */}
           <div className="relative z-0">
             <Contact />
-            {/* <StarsCanvas /> */}
           </div>
         </div>
       </BrowserRouter>
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,8 +6,9 @@ import CanvasLoader from "../Loader";
 
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF('./public/desktop_pc/scene.gltf')
-  // const computer = useGLTF('../../../public/desktop_pc/scene.gltf')
+  // const computer = useGLTF('./public/desktop_pc/scene.gltf') //classiq format
+  const computer = useGLTF('./desktop_pc/scene.gltf') //netlify format
+
 
   return (
     <mesh>
